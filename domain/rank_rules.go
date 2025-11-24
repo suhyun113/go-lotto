50ea42f842b5edfe9f4fce1dca28005d419a8c0e
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-11-24 20:54:56 +0900

    feat: 등수 판정 규칙 및 로직 추가

---
+++ domain/rank_rules.go
@@ -0,0 +1,21 @@
+package domain
+
+import "go-lotto/constants"
+
+func DetermineRank(matchCount int, bonusMatch bool) constants.Rank {
+	switch matchCount {
+	case 6:
+		return constants.RankFirst
+	case 5:
+		if bonusMatch {
+			return constants.RankSecond
+		}
+		return constants.RankThird
+	case 4:
+		return constants.RankFourth
+	case 3:
+		return constants.RankFifth
+	default:
+		return constants.RankNone
+	}
+}
