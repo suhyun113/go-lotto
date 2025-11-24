84386966f60fa6bc6ab9c1aad4a7fc9f03c7a2ca
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-11-24 21:49:50 +0900

    feat: CommaInt64 추가
    - 상금, 총당첨금 등을 출력할 때, 콤마를 찍어주는 용도
    - 결과 출력 가독성 개선

--- util/format.go
+++ util/format.go
@@ -7,6 +7,26 @@ import (
 	"strings"
 )
 
+func CommaInt64(n int64) string {
+	s := strconv.FormatInt(n, 10)
+	if len(s) <= 3 {
+		return s
+	}
+
+	var b strings.Builder
+	pre := len(s) % 3
+	if pre == 0 {
+		pre = 3
+	}
+	b.WriteString(s[:pre])
+
+	for i := pre; i < len(s); i += 3 {
+		b.WriteString(",")
+		b.WriteString(s[i : i+3])
+	}
+	return b.String()
+}
+
 func FormatTicket(numbers []int) string {
 	if len(numbers) == 0 {
 		return "[]"
