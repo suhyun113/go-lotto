9ddc2fa6b4ff47aca68b449ff58c3259922515ce
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-11-24 21:19:25 +0900

    feat: 당첨 통계, 수익률, 총당첨금 출력 추가
    - 당첨 통계 헤더 출력
    - 등수 순서 고정 출력
    - 총 수익률 계산 + FormatRate 출력
    - 총 당첨금 출력

--- view/output_view.go
+++ view/output_view.go
@@ -5,6 +5,7 @@ import (
 
 	"go-lotto/domain"
 	"go-lotto/util"
+	"go-lotto/constants"
 )
 
 type OutputView struct{}
@@ -16,4 +17,36 @@ func (ov *OutputView) PrintTickets(tickets []*domain.Lotto) {
 	for _, t := range tickets {
 		fmt.Println(util.FormatTicket(t.GetNumbers()))
 	}
+}
+
+func (ov *OutputView) PrintEvaluationResults(
+	rankCounts map[constants.Rank]int,
+	totalPrize int64,
+	purchaseAmount int,
+) {
+	fmt.Println("\n당첨 통계")
+	fmt.Println("---")
+
+	order := []constants.Rank{
+		constants.RankFifth,
+		constants.RankFourth,
+		constants.RankThird,
+		constants.RankSecond,
+		constants.RankFirst,
+	}
+
+	for _, r := range order {
+		c := 0
+		if rankCounts != nil {
+			c = rankCounts[r]
+		}
+		fmt.Printf("%s - %d개\n", constants.RankLabel[r], c)
+	}
+
+	rate := 0.0
+	if purchaseAmount > 0 {
+		rate = float64(totalPrize) / float64(purchaseAmount) * 100.0
+	}
+	fmt.Printf("총 수익률은 %s입니다.\n", util.FormatRate(rate))
+	fmt.Printf("(총 당첨금: %s원)\n", util.CommaInt64(totalPrize))
 }
