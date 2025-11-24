5c2ccf547548e90e0060e003ec8785331fff8578
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-11-24 21:17:35 +0900

    feat: 구매 티켓 출력 추가
    - 구매 개수 출력
    - 각 티켓을 FormatTickets 형식으로 출력

--- view/output_view.go
+++ view/output_view.go
@@ -1,5 +1,19 @@
 package view
 
+import (
+	"fmt"
+
+	"go-lotto/domain"
+	"go-lotto/util"
+)
+
 type OutputView struct{}
 
 func NewOutputView() *OutputView { return &OutputView{} }
+
+func (ov *OutputView) PrintTickets(tickets []*domain.Lotto) {
+	fmt.Printf("\n%d개를 구매했습니다.\n", len(tickets))
+	for _, t := range tickets {
+		fmt.Println(util.FormatTicket(t.GetNumbers()))
+	}
+}
