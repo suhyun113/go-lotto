88475b5b4acd81e0121596b61ee207d9f9b7ad52
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-11-24 21:00:14 +0900

    feat: 로또 1장 자동 생성 추가
    - import 추가
    - GenerateTicket 구현

--- service/lotto_service.go
+++ service/lotto_service.go
@@ -3,6 +3,10 @@ package service
 import (
 	"math/rand"
 	"time"
+	"sort"
+
+	"go-lotto/constants"
+	"go-lotto/domain"
 )
 
 type LottoService struct {
@@ -13,4 +17,21 @@ func NewLottoService() *LottoService {
 	return &LottoService{
 		rng: rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
+}
+
+func (s *LottoService) GenerateTicket() (*domain.Lotto, error) {
+	nums := make([]int, 0, constants.LottoSize)
+	used := make(map[int]bool, constants.LottoSize)
+
+	for len(nums) < constants.LottoSize {
+		n := s.rng.Intn(constants.LottoMax-constants.LottoMin+1) + constants.LottoMin
+		if used[n] {
+			continue
+		}
+		used[n] = true
+		nums = append(nums, n)
+	}
+
+	sort.Ints(nums)
+	return domain.NewLotto(nums)
 }
